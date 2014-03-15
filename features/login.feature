89049008407259618b99df9eb03d4db44eeee91e
Daniel Silva <daniel@silvanolte.com>
2014-03-15 13:43:50 -0400

    Fixed sign up and log in cukes.

--- features/login.feature
+++ features/login.feature
@@ -1,5 +1,14 @@
 Feature: Login to Momentum
 
+  Scenario: Register on Momentum
+    Given I go to the Momentum home page
+    When I click on the sign up link
+    When I type "dsilva@ipcoop.com" as my email address
+    When I type "password" as my password
+    When I type "password" as my password confirmation
+    When I click the sign up button
+    Then I am redirected to the home page
+
   Scenario: Login with user on Momentum
     Given I have an account on Momentum
     When I login with my credentials
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,3 +1,32 @@
+Given(/^I go to the Momentum home page$/) do
+  visit '/'
+  find('//div[@id="container"]/h2', :text => 'Sign in')
+end
+
+When(/^I click on the sign up link$/) do
+  click_link 'Sign up'
+end
+
+When(/^I type "(.*?)" as my email address$/) do |email|
+  fill_in 'user[email]', :with => email
+end
+
+When(/^I type "(.*?)" as my password$/) do |password|
+  fill_in 'user[password]', :with => password
+end
+
+When(/^I type "(.*?)" as my password confirmation$/) do |password|
+  fill_in 'user[password_confirmation]', :with => password
+end
+
+When(/^I click the sign up button$/) do
+  click_button 'Sign up'
+end
+
+Then(/^I am redirected to the home page$/) do
+  find('//h1', :text => 'IPC Hackathon Momentum App')
+end
+
 Given(/^I have an account on Momentum$/) do
   visit '/'
   find('//div[@id="container"]/h2', :text => 'Sign in')
@@ -5,10 +34,10 @@ end
 
 When(/^I login with my credentials$/) do
   fill_in 'user[email]', :with => 'dsilva@ipcoop.com'
-  fill_in 'user[password]', :with => 'my-super-secure-password'
+  fill_in 'user[password]', :with => 'password'
   click_button 'Sign in'
 end
 
 Then(/^I should be logged into the system$/) do
-  pending # express the regexp above with the code you wish you had
+  find('//h1', :text => 'IPC Hackathon Momentum App')
 end
