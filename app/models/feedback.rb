6f9f5c88286985bda5c24a563a8b5d7fd0d228ca
Ariel Alonso <aalonso@ipcoop.com>
2014-03-15 16:19:38 -0400

    Added assocation for feedback->author

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,4 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
-  has_one :author
+  belongs_to :author, class_name: 'User'
 end
--- db/migrate/20140315193452_add_column_user_id_to_feedbacks.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnUserIdToFeedbacks < ActiveRecord::Migration
-  def change
-    add_column :feedbacks, :user_id, :integer
-  end
-end
