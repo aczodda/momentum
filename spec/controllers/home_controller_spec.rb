b4df87d48b31efd147c8bbbef6a12a344b2158fd
Ariel Alonso <aalonso@ipcoop.com>
2014-03-15 22:26:23 -0400

    Improved the styling

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 describe HomeController do
 
   describe "GET #index without feedback" do 
-    it "gets current user" do
+    it "sets user to authenticated user" do
+      @user = User.new
+      controller.stub(:authenticate_user!)
+      controller.stub(:current_user).and_return(@user)
+
+      get :index
+
+      expect(assigns(:user)).to eq(@user)
     end
   end
 end
