0a19f4d4dd91b4ca93cf3943dbf29ff426c01439
Ariel Alonso <aalonso@ipcoop.com>
2014-03-16 14:32:50 -0400

    Fix test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,19 +2,33 @@ require 'spec_helper'
 
 describe HomeController do
 
-  describe "GET #index without feedback" do 
+  describe 'GET #index' do
+
     it "sets user to authenticated user" do
-      mock_user = User.new
       mock_users = []
+      mock_user = User.new
 
       User.stub(:all).and_return(mock_users)
       controller.stub(:authenticate_user!)
       controller.stub(:current_user).and_return(mock_user)
 
       get :index
-
       expect(assigns(:user)).to eq(mock_user)
-      expect(assigns(:users)).to eq(mock_users)
     end
+
+    it 'gets index without feedback' do
+      mock_user = User.new
+      controller.stub(:authenticate_user!)
+      controller.stub(:current_user).and_return(mock_user)
+      User.stub(:all).and_return([])
+
+      get :index
+
+      assert_response :success
+      expect(assigns(:user)).to eq(mock_user)
+      expect(assigns(:users)).to eq([])
+    end
+
   end
+  
 end
