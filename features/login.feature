e8e14529da281356dba7ae7d6f52eb7bb74af4ec
Daniel Silva <daniel@silvanolte.com>
2014-03-16 23:49:40 -0400

    Updated cucumber to test registration and login features with latest code.

--- features/login.feature
+++ features/login.feature
@@ -3,14 +3,17 @@ Feature: Login to Momentum
   Scenario: Register on Momentum
     Given I go to the Momentum home page
     When I click on the sign up link
-    When I type "dsilva@ipcoop.com" as my email address
-    When I type "password" as my password
-    When I type "password" as my password confirmation
+    When I type "mickey@disney.com" as my email address
+    And I type "Mickey" as my first name
+    And I type "Mouse" as my last name
+    And I type "password" as my password
+    And I type "password" as my password confirmation
     When I click the sign up button
     Then I am redirected to the home page
 
   Scenario: Login with user on Momentum
-    Given I have an account on Momentum
-    When I login with my credentials
-    Then I should be logged into the system
+    Given I go to the Momentum home page
+    When I login with "mickey@disney.com" as my email address
+    And with "password" as my password
+    Then I am redirected to the home page
 
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -10,6 +10,14 @@ When(/^I type "(.*?)" as my email address$/) do |email|
   fill_in 'user[email]', :with => email
 end
 
+When(/^I type "(.*?)" as my first name$/) do |first_name|
+  fill_in 'user[first_name]', :with => first_name
+end
+
+When(/^I type "(.*?)" as my last name$/) do |last_name|
+  fill_in 'user[last_name]', :with => last_name
+end
+
 When(/^I type "(.*?)" as my password$/) do |password|
   fill_in 'user[password]', :with => password
 end
@@ -23,19 +31,17 @@ When(/^I click the sign up button$/) do
 end
 
 Then(/^I am redirected to the home page$/) do
-  # find('//h1', :text => 'IPC Hackathon Momentum App')
+  find('//h3', :text => 'Mickey Mouse')
+  find('//div[@class="form-group"]/label', :text => 'Send Praise')
+  find('//div[@class="form-group"]/textarea[@name="message[]"]')
+  click_link 'Logout'
 end
 
-Given(/^I have an account on Momentum$/) do
-  visit '/'
+When(/^I login with "(.*?)" as my email address$/) do |email|
+  fill_in 'user[email]', :with => email
 end
 
-When(/^I login with my credentials$/) do
-  fill_in 'user[email]', :with => 'dsilva@ipcoop.com'
-  fill_in 'user[password]', :with => 'password'
+When(/^with "(.*?)" as my password$/) do |password|
+  fill_in 'user[password]', :with => password
   click_button 'Sign in'
-end
-
-Then(/^I should be logged into the system$/) do
-  # find('//h1', :text => 'IPC Hackathon Momentum App')
 end
