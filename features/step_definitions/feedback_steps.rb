73f4d8d8b5bf84c783ae6ad802c41511b11f0c5c
Daniel Silva <daniel@silvanolte.com>
2014-05-20 16:51:55 -0400

    Added new cuke for new feature for CD presentation, also edited bootstrap.css to work properly without giving routing problems.:

---
+++ features/step_definitions/feedback_steps.rb
@@ -0,0 +1,21 @@
+Given(/^I’m browsing the feedback feed$/) do
+  visit('/users/sign_in')
+  fill_in('user_email', :with => 'minnie@disney.com')
+  fill_in('user_password', :with => 'password')
+  click_button('Sign in')
+  find('//h3', :text => 'Minnie Mouse')
+end
+
+Given(/^I see a bit of praise that I agree with$/) do
+  visit('/all_feedback')
+  find('//p', :text => 'Great job!')
+  find('//small', :text => 'Donald Duck to Daisy Duck')
+end
+
+When(/^I click on the “agree” link for that praise$/) do
+  pending
+end
+
+Then(/^I should see my agreement reflected on the page$/) do
+  pending
+end
